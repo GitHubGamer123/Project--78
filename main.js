d60cd4701d9f8f1eefaeb20bccae5da3d85aae42
GitHubGamer123 <85595698+GitHubGamer123@users.noreply.github.com>
2022-03-07 09:10:42 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = [images];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = [names] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
+var images = ["https://as2.ftcdn.net/jpg/01/15/47/87/500_F_115478791_GG0yIcVA8Mhe6MAXEvgcfyL7JtuVfYvm.jpg", "https://lh3.googleusercontent.com/proxy/viy7XjvLGbz3z_OaAyS5c5E6ihzsoMK-mMCr9Hza2QN3-BmwgA1NOmV50Q2wrGNjphTA8OhEP7IDBcZM_wpr3uKAVDDzyLKHHvpyGqA9BQ", "https://previews.123rf.com/images/yupiramos/yupiramos1712/yupiramos171207137/91230755-young-girl-kid-character-standing-cartoon-vector-illustration.jpg", "family.png"];
+var names = ["Mom", "Dad", "Me", "Us"];
+var i = 0;
+
+function update(){
+
+  var array_length = images.length - 1;
+  var updated_image = images[i];
+  var updated_text = names[i];
+
+  document.getElementById("family_book_image").src = updated_image;
+  document.getElementById("name").innerHTML = updated_text;
+
+  i++;
+
+
+  if(i>array_length){
+  i = 0;
+         }
+  }
